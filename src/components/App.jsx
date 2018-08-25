1ece8a55428848d28e1fd7ca543f9514491ed8dd
Oksana Tkachenko <oksana@crystax.net>
2018-08-25 10:34:35 +0300

    installed react-router-dom and added path to first component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const Top = () => <h2>Top</h2>
+const Top2 = () => <h2>Top2</h2>
+const Menu = () => <h2>Menu</h2>
 
 const App = () => {
-  return <h1>Hello from component React</h1>
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path='/' component={Top} />
+        </div>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
