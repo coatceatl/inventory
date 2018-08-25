37f0b9237db77167810e1dd4ca7fda64ade0ea9d
Oksana Tkachenko <oksana@crystax.net>
2018-08-25 14:56:17 +0300

    added content to component top

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -6,26 +6,30 @@ class Top extends Component {
   render() {
     return (
       <nav>
-        <a className='logo' href='#'>
-          <img src='src/img/logo.png' alt='logo' />
-        </a>
-        <ul>
-          <li>
-            <a href='#'>Предметы</a>
-          </li>
-          <li>
-            <a href='#'>Места</a>
-          </li>
-          <li>
-            <a href='#'>Люди</a>
-          </li>
-        </ul>
-        <div className='user'>
-          <div className='user-name'>Людмила Андропова</div>
-          <div className='user-avatar'>
-            <img src='src/img/ava.png' alt='logo' />
+        <div className='top-left col'>
+          <a className='logo' href='#'>
+            <img src='src/img/logo.png' alt='logo' />
+          </a>
+          <div className='stat'>
+            <a href='#'><img src='src/img/stat.png' alt='logo' /></a>
           </div>
         </div>
+        <ul className='col list'>
+          <li className='item'>
+            <a className='link active' href='#'>Предметы</a>
+          </li>
+          <li className='item'>
+            <a className='link' href='#'>Места</a>
+          </li>
+          <li className='item'>
+            <a className='link' href='#'>Люди</a>
+          </li>
+        </ul>
+        <a className='user col' href='#'>
+          <div className='user-name'>Людмила Андропова</div>
+          <img className='avatar' src='src/img/ava.png' alt='logo' />
+          <img src='src/img/arrow.png' alt='logo' />
+        </a>
       </nav>
     )
   }
