e589c5656a1774c41d02adff27587b3fd1497f32
Oksana Tkachenko <oksana@crystax.net>
2018-08-25 12:14:08 +0300

    added content to component top

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -4,9 +4,26 @@ class Top extends Component {
   render() {
     return (
       <nav>
-        <a className='logo'>
+        <a className='logo' href='#'>
           <img src='src/img/logo.png' alt='logo' />
         </a>
+        <ul>
+          <li>
+            <a href='#'>Предметы</a>
+          </li>
+          <li>
+            <a href='#'>Места</a>
+          </li>
+          <li>
+            <a href='#'>Люди</a>
+          </li>
+        </ul>
+        <div className='user'>
+          <div className='user-name'>Людмила Андропова</div>
+          <div className='user-avatar'>
+            <img src='src/img/ava.png' alt='logo' />
+          </div>
+        </div>
       </nav>
     )
   }
