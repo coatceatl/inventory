2d67f3e6c98b1b3b8da7fae5320d810884079c73
Oksana Tkachenko <oksana@crystax.net>
2018-08-29 19:08:21 +0300

    WIP, added search

--- src/components/Top2.jsx
+++ src/components/Top2.jsx
@@ -56,8 +56,10 @@ class Top2 extends Component {
             <Col>
               <Form>
                 <FormGroup>
-                  <Label for="exampleSearch">Search</Label>
-                  <Input type="search" name="search" id="exampleSearch" placeholder="search placeholder" />
+                  <NavLink href='#'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3"><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg>
+                    <Input type="search" name="search" id="exampleSearch" placeholder="Введите название для поиска" />
+                  </NavLink>
                 </FormGroup>
               </Form>
             </Col>
