27bfa8dbefc6b47723876aa08d74c7d2bf81553e
Oksana Tkachenko <oksana@crystax.net>
2018-08-29 13:08:31 +0300

    installed reactstrap bootstrap jquery

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 import Top from './Top.jsx';
+import Top2 from './Top2.jsx';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path='/' component={Top} />
+          <Route path='/' component={Top2} />
         </div>
       </BrowserRouter>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import 'bootstrap';
+import 'jquery';
 
 
 ReactDOM.render(
