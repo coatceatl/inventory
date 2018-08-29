ac78271b81cb7656c85e1d547bf5e388e520af08
Oksana Tkachenko <oksana@crystax.net>
2018-08-29 18:28:58 +0300

    added form select

--- src/components/Top2.jsx
+++ src/components/Top2.jsx
@@ -61,6 +61,20 @@ class Top2 extends Component {
                 </FormGroup>
               </Form>
             </Col>
+            <Col>
+              <Form>
+                <FormGroup>
+                  <Label for="exampleSelect">Select</Label>
+                  <Input type="select" name="select" id="exampleSelect">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </Input>
+                </FormGroup>
+              </Form>
+            </Col>
           </Row>
         </Container>
       </div>
