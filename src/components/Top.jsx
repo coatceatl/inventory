53a0a94a244ab397b2b6860e098e50d05ab8ccbe
Oksana Tkachenko <oksana@crystax.net>
2018-08-29 17:50:39 +0300

    changed component Top, added navbar

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -1,36 +1,70 @@
 import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container,
+    Row,
+    Col,
+} from 'reactstrap';
 
 import '../scss/_top.scss';
 
 class Top extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
     return (
-      <nav>
-        <div className='top-left col'>
-          <a className='logo' href='#'>
+      <div>
+        <Navbar color='inverse' light expand='md'>
+          <NavbarBrand href='#'>
             <img src='src/img/logo.png' alt='logo' />
-          </a>
-          <div className='stat'>
-            <a href='#'><img src='src/img/stat.png' alt='logo' /></a>
-          </div>
-        </div>
-        <ul className='col list'>
-          <li className='item'>
-            <a className='link active' href='#'>Предметы</a>
-          </li>
-          <li className='item'>
-            <a className='link' href='#'>Места</a>
-          </li>
-          <li className='item'>
-            <a className='link' href='#'>Люди</a>
-          </li>
-        </ul>
-        <a className='user col' href='#'>
-          <div className='user-name'>Людмила Андропова</div>
-          <img className='avatar' src='src/img/ava.png' alt='logo' />
-          <img src='src/img/arrow.png' alt='logo' />
-        </a>
-      </nav>
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className='mx-auto' navbar>
+              <NavItem>
+                <NavLink href='#'>
+                  <img src='src/img/stat.png' alt='logo' />
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href='#'>Предметы</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href='#'>Места</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href='#'>Люди</NavLink>
+              </NavItem>
+            </Nav>
+            <Nav className='ml-auto' navbar>
+              <NavItem>
+                <NavLink href='#'>Людмила Андропова
+                  <img src='src/img/ava.png' alt='logo' />
+                  <img src='src/img/arrow.png' alt='logo' />
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
     )
   }
 }
